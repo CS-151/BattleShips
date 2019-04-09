528bc9745d3b33d76fdefd592365b80fba59cd6f
Nathan <Nathan@Nathans-MacBook-Pro-2.local>
2019-04-08 22:48:34 -0700

    Update Player.java

--- Battleships/src/edu/sjsu/cs/cs151/battleship/model/Player.java
+++ Battleships/src/edu/sjsu/cs/cs151/battleship/model/Player.java
@@ -1,5 +1,7 @@
 package edu.sjsu.cs.cs151.battleship.model;
 
+import java.util.ArrayList;
+
 /**
 The player class holds information regarding the number of ships a player has and the location of the ship.
 **/
@@ -14,10 +16,9 @@ public class Player
     **/
     public Player()
     {
-        if (NUM_OF_SHIPS != 5) // Num of ships must be 5
-        {
-            throw new IllegalArgumentException("ERROR! Num of ships must be 5");
-        }
+        if (NUM_OF_SHIPS != 5) {
+			throw new IllegalArgumentException("ERROR! Num of ships must be 5");
+		}
         
         ships = new Ship[NUM_OF_SHIPS];
         for (int i = 0; i < NUM_OF_SHIPS; i++)
@@ -29,48 +30,34 @@ public class Player
         playerGrid = new Grid();
         oppGrid = new Grid();
     }
+
     /**
-    Adds a ship to the array of ships.
-    **/
-    public void addShips()
-    {
-        for (Ship s: ships)
-        {
-            playerGrid.addShip(s);
-        }
-    }
-    /**
-    Returns how many ships are left.
-    @return counter number of ships
-    **/
-    public int numOfShipsLeft()
-    {
-        int counter = 5;
-        for (Ship s: ships)
-        {
-            if (s.isLocationSet() && s.isDirectionSet())
-                counter--;
-        }
-        
-        return counter;
-        
-    }
+ 
     /**
     Chooses the location of the ship.
-    @param s the ship identifier 
+    @param sizeOfShip the ship identifier 
     @param row the row the ship will be in 
     @param col the column that the ship will be
     @param direction the direction the ship will go
     **/
-    public void chooseShipLocation(Ship s, int row, int col, int direction)
+    public void chooseShipLocation(int sizeOfShip, int row, int col, int alignment)
     {
-        s.setLocation(row, col);
-        s.setDirection(direction);
-        playerGrid.addShip(s);
+    	Ship newShip = new Ship(sizeOfShip);
+        playerGrid.addShip(newShip, row, col, alignment);
+        playerGrid.printGrid();
+    }
+    
+    public void guessOpponentShip(int row, int col)
+    {
+    	oppGrid.guessShip(row, col);
+    	oppGrid.setCoordinates(row, col, true);
+    	oppGrid.printOppGrid();
     }
     
     //lengths of all of the ships.
     private static final int[] SHIP_LENGTHS = {2, 3, 3, 4, 5};
     //number of ships 
     private static final int NUM_OF_SHIPS = 5;
+    
+
 }
