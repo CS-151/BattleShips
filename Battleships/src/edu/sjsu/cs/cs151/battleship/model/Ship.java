54c64df18fd3d58e5431bb4bbf482cdcf8e89cc4
Nathan <Nathan@Nathans-MacBook-Pro-2.local>
2019-04-08 22:45:24 -0700

    Update Ship.java

--- Battleships/src/edu/sjsu/cs/cs151/battleship/model/Ship.java
+++ Battleships/src/edu/sjsu/cs/cs151/battleship/model/Ship.java
@@ -4,20 +4,85 @@ package edu.sjsu.cs.cs151.battleship.model;
  * Ship class keeps track of all types of ship.
  */
 public class Ship {
+	
 	/*
 	 * Constructor for the Ship class.
 	 */
-	public Ship() {
+
+	public Ship(int blocks) {
+		this.blocksRemaining = blocks;
+		this.size = blocks;
+		row = -1;
+		col = -1;
+		alignment = HORIZONTAL;
+		
+		//this.shipName = name;
+	}
+	public int getAlignment()
+	{
+		return alignment;
+		
+	}
+	
+	public void setAlignment(int alignment)
+	{
+		this.alignment = alignment;
+	}
+	
+	public int getSize() {
+		return size;
 	}
 
-	/*
-	 * Gets the name of the ship.
-	 * 
-	 * @return shipName the name of the ship
-	 */
-	public String getShipName() {
-		return shipName;
+
+	public void setSize(int size) {
+		this.size = size;
 	}
+
+
+	public int getRow() {
+		return row;
+	}
+
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+
+	public int getCol() {
+		return col;
+	}
+
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+
+	public boolean isHit() {
+		return isHit;
+	}
+
+
+	public void setHit(boolean isHit) {
+		this.isHit = isHit;
+	}
+
+
+	public boolean isMiss() {
+		return isMiss;
+	}
+
+
+	public void setMiss(boolean isMiss) {
+		this.isMiss = isMiss;
+	}
+
+
+	public void setBlocksRemaining(int blocksRemaining) {
+		this.blocksRemaining = blocksRemaining;
+	}
+
 
 	/*
 	 * Gets the size of the ship.
@@ -39,8 +104,12 @@ public class Ship {
 
 	private int size;
 	private int blocksRemaining;
-	private String position;
-	private String shipName;
+	private int row;
+	private int col; 
 	private boolean isHit;
 	private boolean isMiss;
+	private int alignment;
+	
+	final static int  HORIZONTAL = 1;
+	final static int  VERTICAL = -1;
 }
