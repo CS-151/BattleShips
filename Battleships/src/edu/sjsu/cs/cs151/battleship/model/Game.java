6a5c6bc4c779663e0c5e38a9fa6b5051b8496f1e
Nathan-Abegaz <33362510+Nathan-Abegaz@users.noreply.github.com>
2019-04-09 00:37:51 -0700

    Update Game.java

--- Battleships/src/edu/sjsu/cs/cs151/battleship/model/Game.java
+++ Battleships/src/edu/sjsu/cs/cs151/battleship/model/Game.java
@@ -1,12 +1,16 @@
 package edu.sjsu.cs.cs151.battleship.model;
 
 public class Game {
+	
 
 	public static void main(String[] args) {
 				
 		Player p1 = new Player();
-		p1.chooseShipLocation(5, 2, 4, Ship.HORIZONTAL);
-		p1.guessOpponentShip(2, 4);
+		Player p2 = new Player();
+		//p1.chooseShipLocation(5, 2, 4, Ship.HORIZONTAL);
+		 p2.chooseShipLocation(3, 4, 1, Ship.HORIZONTAL);
+		 p1.guessOpponentShip(4, 1, p2);
+
 	}
 
 }
