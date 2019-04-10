7720a160c11255cee834c4cd634b67613d7e92f4
Nathan-Abegaz <33362510+Nathan-Abegaz@users.noreply.github.com>
2019-04-10 13:11:20 -0700

    Update Game.java
    Added Hit/Miss into game model

--- Battleships/src/edu/sjsu/cs/cs151/battleship/model/Game.java
+++ Battleships/src/edu/sjsu/cs/cs151/battleship/model/Game.java
@@ -13,16 +13,18 @@ public class Game {
 		Player p1 = new Player();
 		Player p2 = new Player();
 		
+		int row = 0;
+		int col = 0;
+		int size = 0;
+		String alignment = "";
+		int layout = 0;
+		
 		String done = "no";
 		System.out.println("Player 1: Place your ships");
 		p1.playerGrid.printGrid();
 		while(done.equals("no"))
 		{
-			int row = 0;
-			int col = 0;
-			int size = 0;
-			String alignment = "";
-			int layout = 0;
+
 			
 			System.out.println("Enter size of ship");
 			size = scan.nextInt();
@@ -50,8 +52,72 @@ public class Game {
 			p1.chooseShipLocation(size, row, col, layout);
 			System.out.println("Are you done (yes/no)");
 			done = scan.next();
+		}
+		
+		done  = "no";
+		System.out.println("Player 2: Place your ships");
+		p2.playerGrid.printGrid();
+		while(done.equals("no"))
+		{
+
+			
+			System.out.println("Enter size of ship");
+			size = scan.nextInt();
+			
+			System.out.println("Enter row");
+			row = scan.nextInt();
+			
+			System.out.println("Enter col");
+			col = scan.nextInt();
+			
+			System.out.println("Vertical or Horizontal? (v/h)");
+			alignment = scan.next();
+			alignment = alignment.toLowerCase();
+			
+			if(alignment.equals("h"))
+			{
+				layout = Ship.HORIZONTAL;
+			}
+			else
+			{
+				layout = Ship.VERTICAL;
+			}
+			
+			Ship temp = new Ship(row,col,layout);
+			p2.chooseShipLocation(size, row, col, layout);
+			System.out.println("Are you done (yes/no)");
+			done = scan.next();
+		}
+		
+		 done = "no";
+		while(done.equals("no"))
+		{
+			System.out.println("Player 1: Take a guess");
+			
+			System.out.println("Guess row");
+			row = scan.nextInt();
+			
+			System.out.println("Guess col");
+			col = scan.nextInt();
+			
+			p1.guessOpponentShip(row, col, p2);
+			
+			System.out.println("Player 2: Take a guess");
+			
+			System.out.println("Guess row");
+			row = scan.nextInt();
+			
+			System.out.println("Guess col");
+			col = scan.nextInt();
+			
+			p2.guessOpponentShip(row, col, p1);
+			
+			System.out.println("Are you done (yes/no)");
+			done = scan.next();
+
 
 		}
+
 	
 		//p1.chooseShipLocation(5, 2, 4, Ship.HORIZONTAL);
 //		 p2.chooseShipLocation(3, 4, 1, Ship.HORIZONTAL);
