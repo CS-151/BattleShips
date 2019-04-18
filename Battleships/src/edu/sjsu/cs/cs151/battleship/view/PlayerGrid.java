5b50bf1a74f698b088ae64e98333c630f603978a
Marmari127 <36213942+Marmari127@users.noreply.github.com>
2019-04-18 16:48:07 -0700

    Update PlayerGrid.java

--- Battleships/src/edu/sjsu/cs/cs151/battleship/view/PlayerGrid.java
+++ Battleships/src/edu/sjsu/cs/cs151/battleship/view/PlayerGrid.java
@@ -1,5 +1,42 @@
 package edu.sjsu.cs.cs151.battleship.view;
 
-public class PlayerGrid {
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.Point;
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+public class PlayerGrid extends JPanel {
+	JPanel self;
+	
+	public PlayerGrid() {
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS ));
+		self = new JPanel();
+		self.setLayout(new GridLayout(0, 10));
+		for (int i = 0; i< 10; i++) {
+			for(int j = 0; j<10; j++) {
+				val = getCell();
+				self.add(val);
+			}
+		}
+		this.add(self);
+	}
+	
+	public JPanel getTheComp(Point p) {
+		Component cmp = null;
+		for(Component child : self.getComponents()) {
+			if(child.getBounds().contains(p)) {
+				cmp = child;
+			}
+		}
+		return (JPanel) cmp;
+		
+	}
+	
+	JPanel getCell() {
+		return null;
+	}
+	private JPanel val;
 
 }
