ce4571ca774167cbdb49beb90a6737f2206ccddd
Nathan-Abegaz <33362510+Nathan-Abegaz@users.noreply.github.com>
2019-04-26 14:25:10 -0700

    Update Controller.java

--- Battleships/src/edu/sjsu/cs/cs151/battleship/controller/Controller.java
+++ Battleships/src/edu/sjsu/cs/cs151/battleship/controller/Controller.java
@@ -1,5 +1,17 @@
 package edu.sjsu.cs.cs151.battleship.controller;
 
+import javax.swing.text.View;
+
+import edu.sjsu.cs.cs151.battleship.model.Model;
+
 public class Controller {
 
+	public Controller()
+	{
+		
+	}
+	
+	
+	private Model model;
+	private View view;
 }
