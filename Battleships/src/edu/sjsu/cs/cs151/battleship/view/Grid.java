805f2aed8fe79e278847d7bc07a4b3ea5214e176
Marmari127 <36213942+Marmari127@users.noreply.github.com>
2019-04-30 16:43:48 -0700

    image

--- Battleships/src/edu/sjsu/cs/cs151/battleship/view/Grid.java
+++ Battleships/src/edu/sjsu/cs/cs151/battleship/view/Grid.java
@@ -7,11 +7,14 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import java.awt.Panel;
 
 public class Grid {
 
@@ -22,7 +25,7 @@ public class Grid {
 	{
 		initialize();
 	}
-
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -33,11 +36,14 @@ public class Grid {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 
+		JDialog exitDialog = new JDialog();
+		
+		
 		JPanel North = new JPanel();
 		North.setPreferredSize(new Dimension(500, 150));
 		frame.getContentPane().add(North, BorderLayout.NORTH);
 		North.setLayout(null);
-
+		
 		JButton btnNextPlayer = new JButton("NEXT PLAYER...");
 		btnNextPlayer.setFont(new Font("Bahnschrift", Font.PLAIN, 11));
 		btnNextPlayer.setBounds(362, 11, 112, 23);
@@ -75,16 +81,29 @@ public class Grid {
 		lblOpponent.setHorizontalAlignment(SwingConstants.CENTER);
 		lblOpponent.setBounds(339, 125, 65, 14);
 		North.add(lblOpponent);
+		
+		
 
 		JPanel West = new JPanel();
 		West.setPreferredSize(new Dimension(240, 240));
 		West.setLayout(new GridLayout(10, 10));
+		
+		JLabel westLabel = new JLabel();
+		westLabel.setPreferredSize(new Dimension(240,240));
+		West.add(westLabel);
+		
+		ImageIcon img = new ImageIcon(getClass().getResource("/sea.jpeg"));
+		westLabel.setIcon(img);
+		
+		
 		for (int i = 0; i < 10; i++)
 		{
 			for (int j = 0; j < 10; j++)
 			{
 				JButton b = new JButton();
 				West.add(b);
+				
+
 			}
 		}
 		frame.getContentPane().add(West, BorderLayout.WEST);
@@ -92,12 +111,20 @@ public class Grid {
 		JPanel East = new JPanel();
 		East.setPreferredSize(new Dimension(240, 240));
 		East.setLayout(new GridLayout(10, 10));
+		
+		JLabel eastLabel = new JLabel();
+		eastLabel.setBounds(0, 0, 240, 240);
+		East.add(eastLabel);
+		eastLabel.setIcon(img);
+		
+		
 		for (int i = 0; i < 10; i++)
 		{
 			for (int j = 0; j < 10; j++)
 			{
 				JButton b = new JButton();
 				East.add(b);
+			
 			}
 		}
 		frame.getContentPane().add(East, BorderLayout.EAST);
@@ -111,7 +138,12 @@ public class Grid {
 		btnExit.setFont(new Font("Bahnschrift", Font.PLAIN, 11));
 		btnExit.setBounds(364, 11, 112, 23);
 		South.add(btnExit);
-
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){  
+	            exitMessage.setText("Are you sure you want exit the game? ");  
+			}
+		});
+		
 		JButton b1 = new JButton("");
 		b1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -128,6 +160,8 @@ public class Grid {
 		Grid window = new Grid();
 		window.frame.setVisible(true);
 	}
+	
+	
 
 	private JFrame frame;
 }
