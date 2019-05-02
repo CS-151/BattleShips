c1806318c82519098caee19d93a575197fe69ccd
Nathan-Abegaz <33362510+Nathan-Abegaz@users.noreply.github.com>
2019-05-01 20:56:39 -0700

    Update NextTurn.java

--- Battleships/src/edu/sjsu/cs/cs151/battleship/view/NextTurn.java
+++ Battleships/src/edu/sjsu/cs/cs151/battleship/view/NextTurn.java
@@ -4,6 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -36,20 +39,20 @@ public class NextTurn
 
 		JLabel cannon = new JLabel("");
 		cannon.setBounds(55, 130, 60, 50);
-		Image img = new ImageIcon(cannon.getClass().getResource("/cannonball.png")).getImage();
-		cannon.setIcon(new ImageIcon(img));
+		//Image img = new ImageIcon(cannon.getClass().getResource("/cannonball.png")).getImage();
+		//cannon.setIcon(new ImageIcon(img));
 		pnl_Center.add(cannon);
 
 		JLabel cannon1 = new JLabel("");
 		cannon1.setBounds(369, 130, 60, 50);
-		Image img1 = new ImageIcon(cannon.getClass().getResource("/cannonball.png")).getImage();
-		cannon1.setIcon(new ImageIcon(img1));
+		//Image img1 = new ImageIcon(cannon.getClass().getResource("/cannonball.png")).getImage();
+		//cannon1.setIcon(new ImageIcon(img1));
 		pnl_Center.add(cannon1);
 
 		JLabel cannon2 = new JLabel("");
 		cannon2.setBounds(212, 130, 60, 50);
-		Image img2 = new ImageIcon(cannon.getClass().getResource("/cannonball.png")).getImage();
-		cannon2.setIcon(new ImageIcon(img2));
+		//Image img2 = new ImageIcon(cannon.getClass().getResource("/cannonball.png")).getImage();
+		//cannon2.setIcon(new ImageIcon(img2));
 		pnl_Center.add(cannon2);
 
 		JLabel NextPlayer = new JLabel("Next Player's Turn");
@@ -58,11 +61,12 @@ public class NextTurn
 		NextPlayer.setBounds(87, 211, 309, 43);
 		pnl_Center.add(NextPlayer);
 
-		JButton btnStart = new JButton("Start");
-		btnStart.setFont(new Font("Bahnschrift", Font.PLAIN, 16));
-		btnStart.setBounds(197, 294, 89, 23);
-		btnStart.setForeground(new Color(255, 102, 51));
-		pnl_Center.add(btnStart);
+	    startButton = new JButton("Start");
+		startButton.setFont(new Font("Bahnschrift", Font.PLAIN, 16));
+		startButton.setBounds(197, 294, 89, 23);
+		startButton.setForeground(new Color(255, 102, 51));
+		pnl_Center.add(startButton);
+
 	}
 	
 	public static void main(String[] args) 
@@ -71,5 +75,16 @@ public class NextTurn
 		window.frame.setVisible(true);
 	}
 	
+	public JFrame getFrame()
+	{
+		return frame;
+	}
+	
+	public JButton getStartButton()
+	{
+		return startButton;
+	}
+	
+	public JButton startButton;
 	private JFrame frame;
 }
