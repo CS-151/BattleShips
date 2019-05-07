b51c6092ddf9c55e9ae4bcc2a76a1026387cdd3f
Sarah Supat <43248599+ssupat97@users.noreply.github.com>
2019-05-06 19:25:55 -0700

    Update Welcome.java

--- Battleships/src/edu/sjsu/cs/cs151/battleship/view/Welcome.java
+++ Battleships/src/edu/sjsu/cs/cs151/battleship/view/Welcome.java
@@ -5,11 +5,17 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.Toolkit;
-import javax.swing.Icon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.Timer;
@@ -23,7 +29,7 @@ public class Welcome
 	{
 		initialize();
 	}
-	
+
 	/**
 	 * Creates the frame.
 	 */
@@ -33,64 +39,101 @@ public class Welcome
 		frame.getContentPane().setLayout(null);
 		frame.setBounds(0, 0, 500, 500);
 		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(frame.getClass().getResource("/cannonball.png")));
-		
+
+		JMenu menu = new JMenu("Menu");
+		menu.setBounds(0, 0, 107, 22);
+		frame.add(menu);
+		JMenuBar bar = new JMenuBar();
+
+		m1 = new JMenuItem("About");
+		m1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				try
+				{
+					JOptionPane.showMessageDialog(frame, new ImageIcon("image/About.png"),"About", JOptionPane.INFORMATION_MESSAGE);
+				} catch(Exception e)
+				{
+					JOptionPane.showMessageDialog(null, e);
+				}
+			}
+		});
+
+		m2 = new JMenuItem("Tuturial");
+		m2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				try
+				{
+					JOptionPane.showMessageDialog(frame, new ImageIcon("image/Tutorial.png"),"Tutorial", JOptionPane.INFORMATION_MESSAGE);
+				} catch(Exception e)
+				{
+					JOptionPane.showMessageDialog(null, e);
+				}
+			}
+		});
+
+		m3 = new JMenuItem("Exit");
+		m3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				int n = JOptionPane.showConfirmDialog(frame, "Are you sure you want to exit the game?", "Exit Game", JOptionPane.YES_NO_OPTION);
+				if (n == 0)
+				{
+					try
+					{
+						//Exits the game entirely.
+						System.exit(0);
+					} catch(Exception e)
+					{
+						JOptionPane.showMessageDialog(null, e);
+					}
+				}
+			}
+		});
+
+		menu.add(m1);
+		menu.add(m2);
+		menu.add(m3);
+		bar.add(menu);
+		frame.setJMenuBar(bar);
+
 		//Clouds animated
 		JPanel TOP = new JPanel();
-		TOP.setBounds(0, 0, 484, 100);
-		frame.getContentPane().add(TOP);
-		TOP.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		final Moveable cloud= new ImgShape(0, 0, "/cloud.png");
+		TOP.setBounds(0, 0, 484, 120);
+		TOP.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+		final Moveable cloud= new ImgShape(-50, -50, "/cloud.png");
 		ShapeIcon icon1 = new ShapeIcon(cloud, 500, 100);
 		final JLabel l1 = new JLabel(icon1);
 		TOP.add(l1);
 		frame.getContentPane().add(TOP);
-		
+
 		JPanel CENTER = new JPanel();
-		CENTER.setBounds(0, 99, 484, 208);
+		CENTER.setBounds(0, 120, 484, 109);
 		frame.getContentPane().add(CENTER);
 		CENTER.setLayout(null);
-		
+
 		JLabel lblWelcomeToBattleship = new JLabel("Welcome to Battleship!");
-		lblWelcomeToBattleship.setBounds(84, 5, 315, 31);
-		lblWelcomeToBattleship.setForeground(new Color(0, 102, 204));
-		lblWelcomeToBattleship.setFont(new Font("Magneto", Font.PLAIN, 25));
+		lblWelcomeToBattleship.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWelcomeToBattleship.setBounds(47, 0, 390, 53);
+		lblWelcomeToBattleship.setForeground(new Color(0, 51, 102));
+		lblWelcomeToBattleship.setFont(new Font("Magneto", Font.BOLD, 30));
 		CENTER.add(lblWelcomeToBattleship);
-		
-		start = new JButton("start");
+
+		start = new JButton("Start");
 		start.setForeground(new Color(255, 102, 51));
 		start.setFont(new Font("Bahnschrift", Font.PLAIN, 16));
-		start.setBounds(192, 45, 100, 25);
+		start.setBounds(192, 54, 100, 25);
 		CENTER.add(start);
 
-		about = new JButton("About");
-		about.setForeground(new Color(255, 102, 51));
-		about.setFont(new Font("Bahnschrift", Font.PLAIN, 16));
-		about.setBounds(192, 75, 100, 25);
-		CENTER.add(about);
-
-		tutorial = new JButton("Tutorial");
-		tutorial.setForeground(new Color(255, 102, 51));
-		tutorial.setFont(new Font("Bahnschrift", Font.PLAIN, 16));
-		tutorial.setBounds(192, 105, 100, 25);
-		CENTER.add(tutorial);
-
-		exit = new JButton("Exit");
-		exit.setForeground(new Color(255, 102, 51));
-		exit.setFont(new Font("Bahnschrift", Font.PLAIN, 16));
-		exit.setBounds(192, 135, 100, 25);
-		CENTER.add(exit);
-		
 		JPanel LSHIP = new JPanel();
-		LSHIP.setBounds(0, 307, 115, 83);
+		LSHIP.setBounds(0, 259, 115, 85);
 		frame.getContentPane().add(LSHIP);
 		JLabel shipL = new JLabel("");
 		Image img4 = new ImageIcon(shipL.getClass().getResource("/shipL.png")).getImage();
 		shipL.setIcon(new ImageIcon(img4));
 		LSHIP.add(shipL);		
-		
+
 		//Cannonball animated
 		JPanel CANNONS = new JPanel();
-		CANNONS.setBounds(115, 307, 236, 83);
+		CANNONS.setBounds(115, 259, 236, 85);
 		frame.getContentPane().add(CANNONS);
 		CANNONS.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 		final Moveable ball = new ImgShape(0, 0, "/cannonball.png");
@@ -98,70 +141,85 @@ public class Welcome
 		final JLabel l2 = new JLabel(icon2);
 		CANNONS.add(l2);
 		frame.getContentPane().add(CANNONS);
-		
+
 		JPanel RSHIP = new JPanel();
-		RSHIP.setBounds(351, 307, 133, 83);
+		RSHIP.setBounds(351, 259, 133, 85);
 		frame.getContentPane().add(RSHIP);
 		JLabel shipR = new JLabel("");
 		shipR.setHorizontalAlignment(SwingConstants.RIGHT);
 		Image img5 = new ImageIcon(shipR.getClass().getResource("/shipR.png")).getImage();
 		shipR.setIcon(new ImageIcon(img5));
 		RSHIP.add(shipR);
-		
+
 		JPanel BOTTOM = new JPanel();
-		BOTTOM.setBounds(0, 391, 484, 70);
-		BOTTOM.setBackground(new Color(0, 102, 204));
+		BOTTOM.setBounds(0, 345, 484, 100);
+		BOTTOM.setBackground(new Color(0, 153, 204));
+		//frame.getContentPane().add(TOP);
+		BOTTOM.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
+		final Moveable fish= new ImgShape(10, 0, "/fish.png");
+		ShapeIcon icon3 = new ShapeIcon(fish, 500, 100);
+		final JLabel l3 = new JLabel(icon3);
+		BOTTOM.add(l3);
 		frame.getContentPane().add(BOTTOM);
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
-		//Cloud timer
-		final int DELAY = 50;
+
+		//Cloud and fish timer
+		final int DELAY = 25;
 		Timer t = new Timer(DELAY, event ->{
-			cloud.move();
+			cloud.move(1);
 			l1.repaint();
+
+			fish.move(0);
+			l3.repaint();
 		});
 		t.start();
-		
+
 		//Cannonball timer
 		final int DELAY2 = 1;
 		Timer t2 = new Timer(DELAY2, event ->{
-			ball.move();
+			ball.move(1);
 			l2.repaint();
 		});
 		t2.start();
 	}
-	
+
 	public static void main(String[] args) 
 	{
 		Welcome window = new Welcome();
 		window.frame.setVisible(true);
 	}
-	
+
 	public JFrame getFrame()
 	{
 		return frame;
 	}
-	
+
 	public JButton getStartButton()
 	{
 		return start;
 	}
+
 	public JButton getAboutButton()
 	{
 		return about;
-	}public JButton getTutorialButton()
+	}
+
+	public JButton getTutorialButton()
 	{
 		return tutorial;
-	}public JButton getExitButton()
+	}
+
+	public JButton getExitButton()
 	{
 		return exit;
 	}
-	
+
 	public JButton start;
 	public JButton about;
 	public JButton tutorial;
 	public JButton exit;
+	public JMenuItem m1, m2, m3;
 	private JFrame frame;
 }
