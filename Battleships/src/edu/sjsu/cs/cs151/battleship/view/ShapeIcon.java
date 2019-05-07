e2800e89af8dd27fd6bf61d53e2b38c6e5ce6055
Sarah Supat <43248599+ssupat97@users.noreply.github.com>
2019-05-06 19:37:35 -0700

    Update ShapeIcon.java
    Includes comments

--- Battleships/src/edu/sjsu/cs/cs151/battleship/view/ShapeIcon.java
+++ Battleships/src/edu/sjsu/cs/cs151/battleship/view/ShapeIcon.java
@@ -3,8 +3,17 @@ package edu.sjsu.cs.cs151.battleship.view;
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * An icon that contains a moveable shape.
+ */
 public class ShapeIcon implements Icon
 {
+	/**
+	 * Class constructor.
+	 * @param s the Moveable shape
+	 * @param w width of the shape
+	 * @param h height of the shape
+	 */
 	public ShapeIcon(Moveable s, int w, int h)
 	{
 		shape = s;
@@ -12,16 +21,25 @@ public class ShapeIcon implements Icon
 		height = h;
 	}
 	
+	/**
+	 * Gets the width of the icon.
+	 */
 	public int getIconWidth()
 	{
 		return width;
 	}
 	
+	/**
+	 * Gets the height of the icon.
+	 */
 	public int getIconHeight()
 	{
 		return height;
 	}
 	
+	/**
+	 * Paints the icon.
+	 */
 	public void paintIcon(Component c, Graphics g, int x, int y)
 	{
 		Graphics2D g2 = (Graphics2D) g;
