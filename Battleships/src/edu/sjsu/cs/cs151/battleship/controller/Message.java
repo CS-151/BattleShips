88efd6585f20cdba1983a66798f1230ba1e6ec92
Sarah Supat <43248599+ssupat97@users.noreply.github.com>
2019-05-13 23:29:15 -0700

    Create Message.java

---
+++ Battleships/src/edu/sjsu/cs/cs151/battleship/controller/Message.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cs.cs151.battleship.controller;
+
+import java.io.Serializable;
+
+/**
+ *  Keeps information from View about user actions and can be processed by Valve
+ * 
+ *
+ */
+public abstract class Message implements Serializable
+{
+
+}
